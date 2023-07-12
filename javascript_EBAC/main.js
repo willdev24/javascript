2e1afb67113c75de3f1cab4f42380c40fdbed624
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-11 21:27:49 -0300

    primeiro formulario

---
+++ javascript_EBAC/main.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('form-depositos');
+
+
+ /* essa funcao serve para a pagna nao enviar os arquivos caso o usuario nao digite o nome completo*/ 
+function validanome(nomecompleto){ 
+  
+   const nomearray = nomecompleto.split(' ');
+    return nomearray.length >2;
+}
+
+form.addEventListener('submit', function(e) {
+      let formevalido = false;   
+      e.preventDefault();
+
+  const nomebeneficiario = document.getElementById('nome-beneficiario');
+  const numerocontabnfiario = document.getElementById('numero-conta');
+  const valorconta = document.getElementById('valor');
+  const menssagemsucesso = 'montante de: ${valorconta.value} depositado para o cliente:   ${nomebeneficiario.value}'; 
+
+
+        formevalido = validanome(nomebeneficiario.value)
+    if( formevalido){
+        alert(menssagemsucesso);
+
+        nomebeneficiario.value ='';
+        numerocontabnfiario.value =' ';
+        valorconta.value =' ';
+   
+    }else{alert('o nome nao esta completo')}
+})
+
+
+
+
+console.log(form);
+
