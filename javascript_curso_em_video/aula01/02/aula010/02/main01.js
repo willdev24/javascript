4a1261b2f8f2078a29c836a0f275a56a3f9876aa
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-17 20:15:29 -0300

    exercicios

--- javascript_curso_em_video/aula01/02/aula010/02/main01.js
+++ javascript_curso_em_video/aula01/02/aula010/02/main01.js
@@ -7,3 +7,5 @@ let nome = window.prompt('qual seu nome?')
 window.confirm( nome + " esta gostando do site?")
 
 
+// number.parseInt(n) converter de um numero para um nmero inteiro
+// number.parseFloat(n) converter de um numero para um nmero real
