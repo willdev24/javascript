9818fd56f86e744b9b07ffd41c95593fb5165306
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-18 21:18:48 -0300

    aprendendo o dom

---
+++ javascript_curso_em_video/aula04_DOM/main.js
@@ -0,0 +1,18 @@
+var p1 = document.getElementsByTagName('p')[1]
+
+window.document.write('esta escrito assim :' + p1.innerText)//pega o html sem formatacao
+
+p1.style.color  = 'red'
+
+var corpo = window.document.body
+
+corpo.style.background = 'black'
+
+document.write(p1.innerHTML) //innerhtml pega o html inteiro
+
+/*
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'estou aguardando...' */
+
+var e = document.querySelector('div#msg').style.color = 'pink'
