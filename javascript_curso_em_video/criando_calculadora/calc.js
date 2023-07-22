a08caa3483297ae4503e4913c6ca28621b40cf73
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-22 20:44:43 -0300

    calculadora finalizada

--- javascript_curso_em_video/criando_calculadora/calc.js
+++ javascript_curso_em_video/criando_calculadora/calc.js
@@ -1,42 +1,47 @@
 
-var val = document.getElementById('valorr')
-
-function mudar0(){
-var a = document.getElementById('zero')
-
-    a.style.color = 'pink'
-    var n1 = 0
-    val.innerText = n1 
 
 
+var  a = document.getElementById('zero')
+var  b = document.getElementById('um')
+var  c = document.getElementById('dois')
+var  d = document.getElementById('tres')
+var  e = document.getElementById('quato')
+var  f = document.getElementById('cinco')
+var  g = document.getElementById('seis')
+var  h = document.getElementById('sete')
+
+        function  mudar(num){    
+        a.style.color = 'pink'
+        var val = document.getElementById('valorr').innerHTML;
+    
+        document.getElementById('valorr').innerHTML = val + num
 
 
+    }
+
+function limpar(){
+    var val = document.getElementById('valorr')
+val.innerHTML = ' '
+}
 
 
+function resulte(){
 
+    var val = document.getElementById('valorr').innerHTML;
+    
+    if(val){
+        document.getElementById('valorr').innerHTML = eval(val)
+
+    }else { document.getAnimations('valorr').innerHTML = "indefinido.."
+ }
 }
 
 
 
+/*funcion back(){
+    var val = document.getElementById('valorr').innerHTML;
+    
+    document.getElementById('valorr').innerHTML = val.substring(0, val.length -1)
 
 
-function somar(){
-
-
-}
-
-function subtrair(){
-
-
-}
-
-function multiplicar(){
-
-
-}
-
-function dividir(){
-
-
-
-}
+}*/
