403070869d1e90a3eb3124131de8ed278386373c
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-31 19:03:46 -0300

    salvando

--- javascript_curso_em_video/exercicio_aula05/javascript.js
+++ javascript_curso_em_video/exercicio_aula05/javascript.js
@@ -2,19 +2,23 @@
 function carregar(){
 
 var msg = document.querySelector('div.container')
-var img  = document.getElementById('foto')
-var data = new data()
-var hora = hora.getHours()
+var img  = document.getElementsByClassName('foto')
+//var data = new data()
+var hora = 16
 var a = document.getElementById('relogio')
 
 
-if(hora >= 5 && hora < 20){
+if(hora >= 5 && hora < 16){
 
-    img.src = "tarde.jpg"  
-    a.style.color='pink'
-    a.style.background = 'black'
+    img.src= "foto_manha.png"  
+   
 
-
+} else if(hora >= 16 && hora < 18){
+  //boa tarde
+    img.src= 'foto_tarde.png'
+  
+} else {
+    img.src = 'foto_noite.png'
 }
 
 }
