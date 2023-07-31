a0139624627258ae41311b416fd10db5b00b0342
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-07-31 20:22:00 -0300

    relogio finalizado

--- javascript_curso_em_video/exercicio_aula05/javascript.js
+++ javascript_curso_em_video/exercicio_aula05/javascript.js
@@ -2,23 +2,25 @@
 function carregar(){
 
 var msg = document.querySelector('div.container')
-var img  = document.getElementsByClassName('foto')
-//var data = new data()
-var hora = 16
-var a = document.getElementById('relogio')
+var img  = document.getElementById('imagem')
+const data = new Date();
+let hora = data.getHours();
+var a = document.body
 
-
+msg.innerHTML =`Agora sao ${hora} horas`
 if(hora >= 5 && hora < 16){
 
     img.src= "foto_manha.png"  
-   
+    a.style.background = 'pink'
 
 } else if(hora >= 16 && hora < 18){
   //boa tarde
     img.src= 'foto_tarde.png'
+    a.style.background = 'blue'
   
 } else {
     img.src = 'foto_noite.png'
+    a.style.background = 'red'
 }
 
-}
+}  
