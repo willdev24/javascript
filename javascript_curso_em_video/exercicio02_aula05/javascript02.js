e73f86144dc29be7344e5f2dad9f8eba5a83de78
willy thiago <128657856+willdev24@users.noreply.github.com>
2023-08-03 19:58:40 -0300

    finalizando

--- javascript_curso_em_video/exercicio02_aula05/javascript02.js
+++ javascript_curso_em_video/exercicio02_aula05/javascript02.js
@@ -3,11 +3,13 @@
 function resposta(){
 
     var formu = document.getElementById('data')
-    
+
     var data = new Date()
     var ano = data.getFullYear()
     
     var res = document.querySelector("div.foto")
+   
+     
 
     if(formu.value.length == 0 || Number(formu.value) > ano){
 
@@ -17,20 +19,35 @@ function resposta(){
         var fsex = document.getElementsByName('favorit')
         var idade = ano - formu.value 
         grnero = ''
-          
+        
+        var img = document.createElement('img')
+        img.setAttribute('id','fotos')
+        
+        var mod = document.getElementById('relogio')
+        
+        
        if(fsex[0].checked){
            genero = 'homem'
             
           if(idade < 10){
-            res.innerHTML = `crianca ${genero} de ${idade} anos `
+                img.setAttribute('src','idoso.png')           
+                res.innerHTML = `crianca ${genero} de ${idade} anos `
+                res.appendChild(img)
 
            }else if(idade > 10 && idade < 18){
-            res.innerHTML = `adolescete ${genero} de ${idade} anos `
-         
+                img.setAttribute('src','idoso.png')
+                res.innerHTML = `adolescete ${genero} de ${idade} anos `
+                res.appendChild(img)
+            
            }else if(idade > 17 && idade < 50){
-            res.innerHTML = `adulto ${genero} de ${idade} anos `
-           }else{
+                img.setAttribute('src','cri_homem.png')
+                res.innerHTML = `adulto ${genero} de ${idade} anos `
+                res.appendChild(img)
+        
+        }else{
+            img.setAttribute('src','cri_homem.png')
             res.innerHTML = `idoso ${genero} de ${idade} anos `
+            res.appendChild(img)
            }
 
 
