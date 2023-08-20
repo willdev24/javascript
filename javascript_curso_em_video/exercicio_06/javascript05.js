eae9ae7d9d32daf1738c1ecf788ebc33051fe7a4
willy thiago <willythiagothiago@gmail.com>
2023-08-20 19:55:13 -0400

    contador finalizado

---
+++ javascript_curso_em_video/exercicio_06/javascript05.js
@@ -0,0 +1,49 @@
+function resposta(){
+
+    var inicio = document.getElementById('data01')
+    var final = document.getElementById('data02')
+    var innter = document.getElementById('data03')
+ 
+    var res = document.querySelector('div.foto')
+    
+if(inicio.value.length == 0 && innter.value.length == 0 && final.value.length == 0){
+    window.alert('{ERRO} VERIFIQUE OS DADOS E TENTE NOVAMENTE')
+
+}else{
+
+   var i = Number(inicio.value)
+   var f = Number(final.value)
+   var r = Number(innter.value)
+   var fun = document.getElementById('relogio')
+   
+   
+   
+if(i<f){
+   
+    do{
+        res.innerHTML += `${i} \u{1f449}`
+       
+        i = i + r
+
+    }while( f >= i )
+    res.innerHTML += `\u{1f3c1}`
+    fun.style.height = 500 +'px'
+
+}else{
+   
+    do{ 
+        
+        res.innerHTML += `${i} \u{1f449}`
+        
+    i = i - r
+
+    }while( f <= i )
+    res.innerHTML += `\u{1f3c1}`
+    fun.style.height = 500 +'px'
+
+
+}
+   
+
+}
+}
