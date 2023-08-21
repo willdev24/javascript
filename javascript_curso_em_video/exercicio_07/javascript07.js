fcea6f7dfabe1128f0f583ae66a79d24324c9527
willy thiago <willythiagothiago@gmail.com>
2023-08-21 13:21:22 -0400

    TABUADA DA GABRIELA FINALIZADA

--- javascript_curso_em_video/exercicio_07/javascript07.js
+++ javascript_curso_em_video/exercicio_07/javascript07.js
@@ -8,20 +8,34 @@ window.alert('ERRO!! digite um numero positivo ')
 }else{
     var fundo = document.getElementById('relogio').style.height = 450 +'px'
     var valor = Number(num.value)
-    var tabuada = document.querySelector('div.foto')
+   
+    let tab = document.getElementById('selec') /* essa variavel e para jogar os caractere dentro do select*/
 
 for (let i = valor ; i == valor ; i++){
 for (let j=0 ; j < 10 ; j++){
 let p = i*(j + 1)
-
-tabuada.innerHTML += `${i} x ${j+1} =${p} <br>`
+       
+        var tabuada = document.createElement('option') /*criando o option para listar a tabuada no select*/
+        tabuada.text = `${i} x ${j+1} =${p}`
+        tab.appendChild(tabuada)
+       
+    }
 }
-}
 
 
 }
 
 }
+
+function limpar(){
+let tab = document.getElementById('selec')
+var tabuada = document.createElement('option') 
+var limpa = document.querySelector('div.foto')        
+        tab.innerHTML = ''
+        tab.appendChild()
+
+}
+
 /*
 if( valor == 1){
     tabuada.innerHTML= `1 x 1 = 1  <br> 2 x 1 = 2 <br> 3 x 1 = 3  <br> 4 x 1 = 4  <br> 5 x 1 = 5  <br> 6 x 1 = 6  <br> 7 x 1 = 7 <br> 8 x 1 = 8 <br> 9 x 1 = 9`
