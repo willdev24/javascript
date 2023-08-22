6c58b295d37591ec414b12ef440b1d1f3900ab58
willy thiago <willythiagothiago@gmail.com>
2023-08-22 08:21:31 -0400

    vetor

---
+++ javascript_curso_em_video/vetores/vetores.js
@@ -0,0 +1,5 @@
+let num = [5,8,3]
+num[3]=6 /* primeira forma de adicionar uma variavel no vetor */ 
+num.push(9)/* segunda forma de adicionar uma variavel no vetor */ 
+num.lenght /* contagem dos elementos*/ 
+num.sort() /* ordena os elementos na ordem crescente */ 
